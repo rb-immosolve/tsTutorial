5eb635b231784d82af7a8e5f5e65b49e63abe37c
Rahul Bhatnagar <rb@immosolve.de>
2023-09-11 09:49:52 +0200

    Example types and some functionality around them

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello World!")
+import { Airplane, airplaneHtml } from "./types/airplane"
+import { Menu, printMenu } from "./types/menu"
+
+const airplane: Airplane = {
+    model: "Airbus A380",
+    flightNumber: "A2201",
+    timeOfDeparture: new Date(),
+    timeOfArrival: new Date(),
+    caterer: {
+        name: "Special Food Ltd",
+        address: "484, Some Street, New York",
+        phone: 1452125
+    }
+}
+
+const menu: Menu[] = [
+    { name: "Pizza", cost: 100, chef: "Pizza Chef" },
+    { name: "Burger", cost: 100, chef: "Burger Chef" },
+    { name: "French Fries", cost: 100 },
+    { name: "Hot Dog", cost: 100 },
+]
+
+console.log(airplaneHtml(airplane));
+console.log(printMenu(menu));
